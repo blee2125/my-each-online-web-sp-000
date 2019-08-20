e9b034ae25a56405c08bf6a069ee7dd34026dd83
favorable-device-5158 <blee2125@gmail.com>
2019-08-20 14:55:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,4 @@
-array = ['arel', 'jon', 'logan', 'spencer']
+
 def my_each(array) # put argument(s) here
   numb = 0
   while numb < array.length
@@ -6,5 +6,5 @@ def my_each(array) # put argument(s) here
     numb +=1
 
   end
-
+  array
 end
